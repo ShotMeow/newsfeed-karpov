0c118a02cd7723b3858120f5e9b454b123b4eefc
ShotMeow <shotmeow@gmail.com>
2022-09-28 19:00:08 +0300

    Add Real Data from Backend

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -1,10 +1,4 @@
-const data = {"sources":[{"id":1,"name":"meduza"},{"id":6,"name":"3dnews"},{"id":7,"name":"nytimes"},{"id":8,"name":"forbes"},{"id":9,"name":"igromania"},{"id":10,"name":"buro237"},{"id":11,"name":"rusvesna"},{"id":12,"name":"7ya"},{"id":14,"name":"aif"},{"id":15,"name":"gazetaru"},{"id":16,"name":"karpov.courses"}],"categories":[{"id":1,"name":"tech"},{"id":2,"name":"sport"},{"id":3,"name":"fashion"},{"id":4,"name":"politics"},{"id":5,"name":"other"},{"id":6,"name":"karpov.courses"}],"items":[{"id":232872,"lang":"ru","date":"Mon Sep 19 2022 15:34:08 GMT+0300 (Moscow Standard Time)","title":"СМИ: гражданина РФ подозревают в убийстве родителей на отдыхе в Анталье","description":"Сообщается, что подозреваемый может иметь психические расстройства.","image":"https://aif-s3.aif.ru/images/029/381/6eb7c45c83d033152d2ee7ae9d2a3192.jpg","source_id":14,"category_id":5},{"id":232879,"lang":"ru","date":"Mon Sep 19 2022 15:26:57 GMT+0300 (Moscow Standard Time)","title":"Врач рассказала, как защитить ребенка от болезней в детсаду","description":"Допустимое количество эпизодов ОРЗ в год для детей составляет восемь случаев.","image":"https://aif-s3.aif.ru/images/029/381/f6a81d184fc962b28ee9b7b829f4cff9.jpg","source_id":14,"category_id":5},{"id":232870,"lang":"ru","date":"Mon Sep 19 2022 15:25:42 GMT+0300 (Moscow Standard Time)","title":"«Глобус» и Synergetic установили фандоматы для сбора тары","description":"Сеть гипермаркетов «Глобус» запустила проект совместно с производителем экологичных моющих средств и натуральной косметики Synergetic. Производством и обслуживанием фандоматов занимается компания Ecoplatform. Проект реализуется сетью «Глобус» в рамках корпоративной программы...","image":"https://www.buro247.ru/images/drozdova2/98877777_3.png","source_id":10,"category_id":5},{"id":232885,"lang":"ru","date":"Mon Sep 19 2022 15:24:50 GMT+0300 (Moscow Standard Time)","title":"Садьдо после долгой болезни вернулся к руководству Херсонской областью","description":"Политик отсутствовал полтора месяца.","image":"https://aif-s3.aif.ru/images/029/381/b90f7e74e02986d0527d5768c1d06925.jpg","source_id":14,"category_id":5},{"id":232871,"lang":"ru","date":"Mon Sep 19 2022 15:21:06 GMT+0300 (Moscow Standard Time)","title":"Хинштейн заявил о поддержке Минюстом введения ответственности за «пропаганду» ЛГБТ","description":"Депутат Александр Хинштейн сообщил, что Минюст подготовил положительный отзыв на его законопроект об административной ответственности за «пропаганду нетрадиционных сексуальных отношений». Теперь документ будет направлен в правительство для рассмотрения","image":"","source_id":8,"category_id":5},{"id":232888,"lang":"ru","date":"Mon Sep 19 2022 15:20:19 GMT+0300 (Moscow Standard Time)","title":"YouTube удалил ролик Слуцкого о давлении украинских властей на педагогов","description":"Видеохостинг объяснил свои действия нарушением правил в отношении дискриминационных высказываний.","image":"https://aif-s3.aif.ru/images/029/381/f8488f0018d70d7af6c191f3ff2a7319.jpg","source_id":14,"category_id":4},{"id":232874,"lang":"ru","date":"Mon Sep 19 2022 15:16:51 GMT+0300 (Moscow Standard Time)","title":"Разработчик игры Pathfinder опроверг информацию об эксклюзивной сделке с VK Play","description":"Компания OwlCat Games, разработчик игры Pathfinder, заявила, что не ведет переговоры с VK об эксклюзивном доступе к ее играм на платформе VK Play. Ранее «Ведомости» сообщили, что IT-компания рассчитывает создать российский аналог Steam и договаривается с разработчиками об эксклюзивных правах на игры","image":"","source_id":8,"category_id":5},{"id":232867,"lang":"ru","date":"Mon Sep 19 2022 15:15:00 GMT+0300 (Moscow Standard Time)","title":"Демоны и кровавые битвы в новом трейлере аниме «Человек-бензопила»","description":"Премьера проекта намечена на 11 октября, в первом сезоне будет 12 эпизодов.","image":"https://cdn.igromania.ru/mnt/news/9/e/7/9/4/4/119349/8060aea9a580be6c_848x477.jpg","source_id":9,"category_id":1},{"id":232889,"lang":"ru","date":"Mon Sep 19 2022 15:12:01 GMT+0300 (Moscow Standard Time)","title":"Инфекционист заявил, что пока преждевременно говорить об окончании пандемии","description":"Сергей Вознесенский заявил, что нельзя точно предсказать поведение ковида через месяц или год.","image":"https://aif-s3.aif.ru/images/029/381/0ef211af798c7033f69d1e41fbc664a3.jpg","source_id":14,"category_id":4},{"id":232890,"lang":"ru","date":"Mon Sep 19 2022 14:56:57 GMT+0300 (Moscow Standard Time)","title":"Мантуров: продукты не будут ввозить в РФ по параллельному импорту","description":"Также не планируется дополнительно обозначать товары, которые ввезены таким способом.","image":"https://aif-s3.aif.ru/images/029/381/4fc0ef964f9aa5a596379859896aa48b.jpg","source_id":14,"category_id":4},{"id":232891,"lang":"ru","date":"Mon Sep 19 2022 14:55:09 GMT+0300 (Moscow Standard Time)","title":"Среди 13 погибших после обстрела Донецка со стороны ВСУ были двое детей","description":"Атака совершена 19 сентября на площади Бакинских Комиссаров в Куйбышевском районе.","image":"https://aif-s3.aif.ru/images/029/381/623f6fa8880e2bf440106ee7d41f546b.jpg","source_id":14,"category_id":5},{"id":232869,"lang":"ru","date":"Mon Sep 19 2022 14:51:16 GMT+0300 (Moscow Standard Time)","title":"Титул Камиллы: как самая ненавидимая женщина Великобритании стала королевой-консортом","description":"8 сентября, в день смерти Елизаветы II, ее сын Чарльз стал королем, а его супруга — королевой-консортом. О том, что для Камиллы Паркер-Боулз — давней возлюбленной Чарльза и вынужденной соперницы принцессы Дианы — значит ее новый титул, рассказывает корреспондент Forbes Woman в Великобритании Илья Гончаров","image":"","source_id":8,"category_id":5},{"id":232875,"lang":"ru","date":"Mon Sep 19 2022 14:51:00 GMT+0300 (Moscow Standard Time)","title":"Автор «Андора» принципиально не хотел добавлять в сериал фансервис","description":"По его словам, знакомые персонажи появляются не просто так, а работают на историю.","image":"https://cdn.igromania.ru/mnt/news/6/b/c/4/2/3/119348/0a71b351811e8bbf_848x477.jpg","source_id":9,"category_id":1},{"id":232892,"lang":"ru","date":"Mon Sep 19 2022 14:50:54 GMT+0300 (Moscow Standard Time)","title":"Центробанк ужесточит требования к ипотеке с низким первоначальным взносом","description":"Все надбавки по таким кредитам были отменены в марте 2022 года.","image":"https://aif-s3.aif.ru/images/029/381/a9e428e3aac7fe000dcc677fb64fad50.jpg","source_id":14,"category_id":5},{"id":232878,"lang":"ru","date":"Mon Sep 19 2022 14:40:52 GMT+0300 (Moscow Standard Time)","title":"Китайцам рекомендовали не касаться иностранцев из-за риска заражения обезьяньей оспой","description":"Китайский чиновник рекомендовал не прикасаться к иностранцам и людям, недавно приехавшим из-за границы, для снижения риска заражения оспой обезьян. Как пишет Bloomberg, среди местных экспатов эту рекомендацию сочли оскорбительной. На прошлой неделе в Китае выявили первого заболевшего обезьяньей оспой: им стал 29-летний торговый агент, который вернулся из Европы","image":"","source_id":8,"category_id":5},{"id":232894,"lang":"ru","date":"Mon Sep 19 2022 14:38:40 GMT+0300 (Moscow Standard Time)","title":"Эксперт раскрыл цель визита Пелоси в Армению","description":"Константин Затулин считает, что Запад хочет «вытеснить» Россию с Кавказа.","image":"https://aif-s3.aif.ru/images/029/380/c8ac9f10ba410f4f60e685b8d3c79a1d.JPG","source_id":14,"category_id":4},{"id":232877,"lang":"ru","date":"Mon Sep 19 2022 14:38:40 GMT+0300 (Moscow Standard Time)","title":"Оганесян, Волобуев и Тодоровский: главные премьеры фестиваля «Новый сезон»","description":"В воскресенье на курорте «Роза Хутор» стартовал первый фестиваль онлайн-кинотеатров «Новый сезон», который продлится до 23 сентября. Проекты представят главные отечественные стриминги. В программе — семь фильмов и 14 сериалов. Постоянный автор BURO. Катя Загвоздкина...","image":"https://www.buro247.ru/images/Alina/1200x630Newseason.gif","source_id":10,"category_id":3},{"id":232896,"lang":"ru","date":"Mon Sep 19 2022 14:37:26 GMT+0300 (Moscow Standard Time)","title":"Минюст поддержал проект закона об ответственности за ЛГБТ-пропаганду","description":"Далее документ будет рассматривать комиссия правительства по законопроектной деятельности.","image":"https://aif-s3.aif.ru/images/029/380/6ab833dd664987608a00cc0e3b15e8c0.jpg","source_id":14,"category_id":5},{"id":232876,"lang":"ru","date":"Mon Sep 19 2022 14:35:01 GMT+0300 (Moscow Standard Time)","title":"ЦБ ужесточит требования к ипотеке на новостройки с низким первоначальным взносом","description":"ЦБ объявил об ужесточении требований к ипотеке на новостройки с первоначальным взносом меньше 10%. Новые правила призваны ограничить выдачу кредитов без первоначального взноса, снизив риски для банковского сектора","image":"","source_id":8,"category_id":5},{"id":232897,"lang":"ru","date":"Mon Sep 19 2022 14:34:58 GMT+0300 (Moscow Standard Time)","title":"В МИД РФ состоялась закрытая встреча Лаврова с главами дипмиссий ряда стран","description":"В частности, ведомство посетили посол Франции Пьер Леви и глава китайского диппредставительства Чжан Ханьхуэй.","image":"https://aif-s3.aif.ru/images/029/380/f8e5d245d98d36f790ebc4ca998438c3.jpg","source_id":14,"category_id":5}]}
-
-const mainNews = data.items.slice(0, 3)
-const smallNews = data.items.slice(3, 12)
-
-const mainNewsTemplate = document.getElementById('main-news-item')
-const smallNewsTemplate = document.getElementById('small-news-item')
+let data = {}
 
 const mainNewsContainer = document.querySelector('.news__main-column')
 const smallNewsContainer = document.querySelector('.news__second-column')
@@ -21,43 +15,52 @@ const escapeString = (string) => {
     })
 }
 
-mainNews.forEach((item) => {
-    const template = document.createElement("template")
-    const source = data.sources.find((source) => source.id === item.source_id)
-    const category = data.categories.find((category) => category.id === item.category_id)
+const renderNews = categoryId => {
+    fetch(`https://frontend.karpovcourses.net/api/v2/ru/news/${categoryId ? categoryId : ''}`)
+        .then(response => response.json())
+        .then(responseData => {
+            data = responseData
+            const mainNews = data.items.slice(0, 3)
+            const smallNews = data.items.slice(3, 12)
 
-    template.innerHTML = `
-        <article class="news-item">
-            <div class="news-item__image-container"><img class="news-item__image" src="${encodeURI(item.image)}" alt="Картинка"></div>
-            <div class="news-item__about">
-                <span class="news-item__category">${escapeString(category.name)}</span>
-                <h2 class="news-item__title">${escapeString(item.title)}</h2>
-                <p class="news-item__description">${escapeString(item.description)}</p>
-                <span class="news-item__source">${escapeString(source.name)}</span>
-            </div>
-        </article>
-    `
+            mainNews.forEach((item) => {
+                const template = document.createElement("template")
+                const source = data.sources.find((source) => source.id === item.source_id)
+                const category = data.categories.find((category) => category.id === item.category_id)
 
-    mainNewsContainer.appendChild(template.content)
-})
+                template.innerHTML = `
+                <article class="news-item">
+                    <div class="news-item__image-container"><img class="news-item__image" src="${encodeURI(item.image)}" alt="Картинка"></div>
+                    <div class="news-item__about">
+                        <span class="news-item__category">${escapeString(category.name)}</span>
+                        <h2 class="news-item__title">${escapeString(item.title)}</h2>
+                        <p class="news-item__description">${escapeString(item.description)}</p>
+                        <span class="news-item__source">${escapeString(source.name)}</span>
+                    </div>
+                </article>
+                `
 
-smallNews.forEach((item) => {
-    const template = document.createElement("template")
-    const source = data.sources.find((source) => source.id === item.source_id)
-    const date = new Date(item.date).toLocaleDateString('ru-RU', {
-        month: 'long',
-        day: 'numeric'
-    })
+                mainNewsContainer.appendChild(template.content)
+                })
 
-    template.innerHTML = `
-        <article class="aside-item">
-            <h3 class="aside-item__title">${escapeString(item.title)}</h3>
-            <div class="aside-item__wrapper">
-                <span class="aside-item__date">${date}</span>
-                <span class="aside-item__source">${escapeString(source.name)}</span>
-            </div>
-        </article>
-    `
+                smallNews.forEach((item) => {
+                    const template = document.createElement("template")
+                    const source = data.sources.find((source) => source.id === item.source_id)
+                    const date = new Date(item.date).toLocaleDateString('ru-RU', {
+                        month: 'long',
+                        day: 'numeric'
+                    })
 
-    smallNewsContainer.appendChild(template.content)
-})
+                            template.innerHTML = `
+                    <article class="aside-item">
+                        <h3 class="aside-item__title">${escapeString(item.title)}</h3>
+                        <div class="aside-item__wrapper">
+                            <span class="aside-item__date">${date}</span>
+                            <span class="aside-item__source">${escapeString(source.name)}</span>
+                        </div>
+                    </article>
+                `
+                smallNewsContainer.appendChild(template.content)
+                })
+        })
+}
