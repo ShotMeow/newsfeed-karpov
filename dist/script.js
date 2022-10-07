e71d87a6d06108aad2890689fc1b19ff44c3a888
ShotMeow <shotmeow@gmail.com>
2022-10-07 16:57:33 +0300

    Delete .dist

--- dist/script.js
+++
@@ -1,5 +0,0 @@
-import { App } from "./components/App.js";
-import React from 'react';
-import ReactDOM from 'react-dom';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( /*#__PURE__*/React.createElement(App, null));
